db1e9a2ebed51b2bf6fd12ed347c61a0b2b8a14e
Hollow4U <131100806+Hollow4U@users.noreply.github.com>
2024-04-16 18:23:25 -0500

    Potions hijo de Items [David Cardenas]

---
+++ ExamenTienda/ExamenTienda/Potions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenTienda
+{
+    class Potions : Items
+    {
+
+        public Potions(string name, float price : base(string name, float price))
+        {
+            this.name = name;
+            this.price = price;
+
+        }
+
+        public virtual float GetName()
+        {
+            Console.WriteLine("Selecciona el nombre de tu pocion");
+            name = Console.ReadLine();
+
+            return name;
+
+        }
+
+        public virtual float GetPrice()
+        {
+            Console.WriteLine("Selecciona el precio de la pocion");
+            price = float.Parse(Console.ReadLine());
+
+            return price;
+        }
+    }
+}
