e57108c9403c7e435b57e9fd6aa4a5177a4d07d1
Hollow4U <131100806+Hollow4U@users.noreply.github.com>
2024-04-16 17:18:37 -0500

    Clase padre Personajes terminada [David Cardenas]

--- ExamenTienda/ExamenTienda/Characters.cs
+++ ExamenTienda/ExamenTienda/Characters.cs
@@ -8,5 +8,27 @@ namespace ExamenTienda
 {
     class Characters
     {
+        protected string name;
+        protected float hp;
+        protected float dmg;
+        protected float ac;
+        protected List <Items> inventory;
+
+        public Characters(string name, float hp, float dmg, float ac)
+        {
+            this.name = name;
+            this.hp = hp;
+            this.dmg = dmg;
+            this.ac = ac;
+            inventory = new List<Items>();
+        }
+
+
+        public void addItem()
+        {
+            inventory.Add(new Items(Items.Getname())
+        }
+
+
     }
 }
