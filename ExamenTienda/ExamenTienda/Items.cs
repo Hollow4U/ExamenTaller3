d67dd5be0f2bb6b866cd940fd1643d7becd0984b
Hollow4U <131100806+Hollow4U@users.noreply.github.com>
2024-04-16 18:14:52 -0500

    Padre Items [David Cardenas]

---
+++ ExamenTienda/ExamenTienda/Items.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenTienda
+{
+    class Items
+    {
+        protected string name;
+        protected float price;
+
+        public Items(string name, float price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
