6bb28a10df88874d2a4f09544e99c8330ba07998
Hollow4U <131100806+Hollow4U@users.noreply.github.com>
2024-04-16 18:20:52 -0500

    Weapons hijo de Items

---
+++ ExamenTienda/ExamenTienda/Weapons.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenTienda
+{
+    class Weapons : Items
+    {
+        {
+
+        public Weapons(string name, float price : base(string name, float price))
+        {
+            this.name = name;
+            this.price = price;
+
+        }
+
+        public virtual float GetName()
+        {
+            Console.WriteLine("Selecciona el nombre de tu arma");
+            name = Console.ReadLine();
+
+            return name;
+
+        }
+
+        public virtual float GetPrice()
+        {
+            Console.WriteLine("Selecciona el precio del arma");
+            price = float.Parse(Console.ReadLine());
+
+            return price;
+        }
+
+    }
+}
+
